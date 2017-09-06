5303fd6a2d14957f023ae543b4baf4695dcc3daa
JyNeo <1322893818@qq.com>
2017-09-06 09:06:57 +0800

    update Two Sum.java

--- 001. Two Sum/Two Sum.java
+++ 001. Two Sum/Two Sum.java
@@ -14,7 +14,7 @@
 class Solution {
 	// Approach #1 (Brute Force)
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+    	int[] result = new int[2];
         for (int i = 0; i < nums.length(); i++) {
         	for (int j = i + 1; j < nums.length; j++) {
         		if (nums[i] + nums[j] == target) {
@@ -26,13 +26,14 @@ class Solution {
         return result;
     }
 
-    // Approach #2 (Two-pass Hash Table)
+	// Approach #2 (Two-pass Hash Table)
 	public int[] twoSum(int[] numbers, int target) {
 		int[] result = new int[2];
 		Map<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < numbers.length; i++) {
 			map.put(numbers[i], i);
 		}
+
 		for (int i = 0; i < numbers.length; i++) {
 			int complement = target - numbers[i];
 			if (map.containsKey(complement) && map.get(complement) != i) {
@@ -46,41 +47,45 @@ class Solution {
 
 	// Approach #3 (One-pass Hash Table)
 	public int[] twoSum(int[] numbers, int target) {
-    	int[] result = new int[2];
-    	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    	for (int i = 0; i < numbers.length; i++) {
-    	    if (map.containsKey(target - numbers[i])) {
-        	    result[1] = i;
-        	    result[0] = map.get(target - numbers[i]);
-            	return result;
-        	}
-        	map.put(numbers[i], i);
-    	}
-    	return result;
+		int[] result = new int[2];
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			if (map.containsKey(target - numbers[i])) {
+				result[1] = i;
+				result[0] = map.get(target - numbers[i]);
+				return result;
+			}
+			map.put(numbers[i], i);
+		}
+		return result;
 	}
 
 	// Approach #4 (Sorting With Two Pointers)
 	public int[] twoSum(int[] numbers, int target) {
-    	int[] result = new int[2];
+		int[] result = new int[2];
 		int[] nums = new int[numbers.length];
 		for (int i = 0; i < numbers.length; i++) { //复制数组
 			nums[i] = numbers[i];
 		}
-    	if(nums == null || nums.length < 2)
+
+		if(nums == null || nums.length < 2)
         	return null;
+
     	Arrays.sort(nums);//数组排序
     	int left = 0;
     	int right = nums.length - 1;
     	while(left < right){
-        	if(nums[left] + nums[right] == target){
-				for (int i = 0; i < numbers.length; i++) {
-					if(numbers[i] == nums[left])
-						result[0] = i;
+    		if(nums[left] + nums[right] == target){
+    			for (int i = 0; i < numbers.length; i++) {
+    				if(numbers[i] == nums[left])
+    					result[0] = i;
 				}
+
 				for (int i = 0; i < numbers.length; i++) {
 					if(numbers[i] == nums[right] && i != result[0]) //避免查找到两个相同元素
 						result[1] = i;
 				}
+
 				if (result[0] > result[1]) { //保证找到的第一个整数索引比第二个要小
 					int temp = result[0];
 					result[0] = result[1];
@@ -89,10 +94,10 @@ class Solution {
             	return result;
         	}
         	else if(nums[left] + nums[right] > target)
-            	right--;
+        		right--;
         	else
-            	left++;
-    	}
+        		left++;
+        }
     	return null;
-	}
+    }
 }
